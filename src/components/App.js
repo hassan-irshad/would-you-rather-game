f0ede4e4fb661d646a11c17cccb627894ffba523
hassan177 <hassanansari17@live.com>
2019-08-12 23:37:43 +0500

    Added basename in Router

--- src/components/App.js
+++ src/components/App.js
@@ -17,7 +17,7 @@ class App extends Component {
   render() {
     return (
       <div> 
-        <Router>
+        <Router basename="/would-you-rather-game">
               <PrivateRoute path="/" exact component={ Home }/> 
               <PrivateRoute path="/add" component={ QuestionForm }/>
               <PrivateRoute path="/leaderboard" component={ Leaderboard }/>
