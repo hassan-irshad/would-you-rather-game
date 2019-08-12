d98205b70d2811534b65f4a18b68105775d14875
hassan177 <hassanansari17@live.com>
2019-08-12 23:04:02 +0500

    Updates

---
+++ precache-manifest.8fd1bc6e257999c32c2e7a853d12d97d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f74bcbf071035939f94f765facab5d4e",
+    "url": "/would-you-rather-game/index.html"
+  },
+  {
+    "revision": "f2307474b83d469462f5",
+    "url": "/would-you-rather-game/static/css/main.27ebfafb.chunk.css"
+  },
+  {
+    "revision": "686e5ffdfb9b3f1841f7",
+    "url": "/would-you-rather-game/static/js/2.715a75c5.chunk.js"
+  },
+  {
+    "revision": "f2307474b83d469462f5",
+    "url": "/would-you-rather-game/static/js/main.4843fb2f.chunk.js"
+  },
+  {
+    "revision": "1c24279e0be1a4442660",
+    "url": "/would-you-rather-game/static/js/runtime~main.5122bc57.js"
+  }
+]);
